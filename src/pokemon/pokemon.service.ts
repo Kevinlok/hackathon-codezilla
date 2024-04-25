5511473195a20d0fad364f1afa42dc2b33802431
Kevin Lok <kevin.lok@rabobank.nl>
2024-04-25 14:55:56 +0200

    updated service and generateCard

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -2,6 +2,20 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { map } from 'rxjs';
 
+export interface Pokemon {
+  name: string;
+  hp: number;
+  moves: {
+    power: number;
+    cost: number;
+    name: string;
+  }[];
+  type: string;
+  ability: string;
+  image: string;
+  retreatCost: number;
+}
+
 @Injectable()
 export class PokemonService {
   constructor(private httpService: HttpService) {}
@@ -13,7 +27,7 @@ export class PokemonService {
       .pipe(map((response) => this.createPokemonEntity(response)));
   }
 
-  createPokemonEntity(pokemon: any) {
+  createPokemonEntity(pokemon: any): Pokemon {
     const randomizedMoves = pokemon.moves
       .map((move) => move.move.name)
       .sort(() => Math.random() - 0.5)
@@ -22,10 +36,30 @@ export class PokemonService {
     return {
       name: pokemon.species.name,
       hp: pokemon.base_experience,
-      moves: randomizedMoves,
+      moves: randomizedMoves.map((move) => this.createMoveEntity(move)),
       type: pokemon.types[0].type.name,
       ability: pokemon.abilities[0].ability.name,
       image: pokemon.sprites.other['official-artwork'].front_default,
+      retreatCost: Math.floor(Math.random() * 3) + 1,
     };
+  }
+
+  createMoveEntity(move: any) {
+    return {
+      name: move,
+      cost: this.getCost(Math.floor(Math.random() * 3) + 1),
+      power: Math.floor(Math.random() * 999),
+    };
+  }
+
+  getCost(cost: number) {
+    switch (cost) {
+      case 1:
+        return '*';
+      case 2:
+        return '**';
+      case 3:
+        return '***';
+    }
   }
 }
